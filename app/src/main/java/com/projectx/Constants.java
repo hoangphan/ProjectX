df9a95514a567ede055c83274e2cddbf9d8e8b56
Hoang Phan <thanh.hoang.bk@gmail.com>
2017-11-11 17:02:07 +0700

    add new files - reference from hud demo - library for maneuver map

---
+++ app/src/main/java/com/projectx/Constants.java
@@ -0,0 +1,17 @@
+package com.projectx;
+
+public class Constants {
+
+  public static final double MPH_DOUBLE = 2.2369;
+  public static final String MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoiaG9hbmdwaGFuIiwiYSI6ImNqOHN2eXY0ZzBlYmczMnAwbTQyNXVhYWkifQ.BbvO9XZQoo0P8qptKXAcVw";
+  public static final String PLACE_LOCATION_EXTRA = "PLACE_LOCATION_EXTRA";
+
+  public static final String DAYS = " days ";
+  public static final String HOUR = " hr ";
+  public static final String MINUTE = " min ";
+  public static final String SECONDS = " seconds";
+  public static final String MILES = " mi";
+  public static final double METER_MULTIPLIER = .00062137;
+  public static final String DURATION_ILLEGAL_ARGUMENT = "Duration must be greater than zero.";
+  public static final int STRING_BUILDER_CAPACITY = 64;
+}
