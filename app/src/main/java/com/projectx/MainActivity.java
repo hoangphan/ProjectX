293006a8b66924d55543ea76cb76ea2b18582c29
Tri Hua <tri2510@gmail.com>
2017-11-28 09:02:02 +0700

    Enable ConnectionActivity as first activity

--- app/src/main/java/com/projectx/MainActivity.java
+++ app/src/main/java/com/projectx/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.projectx.connect.ConnectionActivity;
 import com.projectx.maps.MapsActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     public void startButtonClicked(View view)
     {
         //Create an intent to open connection activity
-        Intent map_activity = new Intent(this, MapsActivity.class);
+        Intent map_activity = new Intent(this, ConnectionActivity.class);
         //Start connection activity
         startActivity(map_activity);
 
